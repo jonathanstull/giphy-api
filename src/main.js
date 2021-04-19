09bd1ddcb94b9c3dbf1ff2fdfaa449d3ffca5e4c
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-19 11:14:29 -0700

    add HTML with search input and API example return
    Co-authored-by: Marney Mallory <marney.mallory@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,3 +3,4 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+// API URL template: api.giphy.com/v1/gifs/`${search+terms}`&api_key=YOUR_API_KEY&limit=20
