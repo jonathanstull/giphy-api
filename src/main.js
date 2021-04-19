354c3f9d78628f9e7a5b9f824a80f65f3c949336
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-19 11:56:05 -0700

    add API request script to main.js
    Co-authored-by: Marney Mallory <marney.mallory@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,3 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 // API URL template: api.giphy.com/v1/gifs/`${search+terms}`&api_key=YOUR_API_KEY&limit=20
+// start with a successful search for just one word
+
+$(document).ready(() => {
+  $('#search').click(() => {
+    const searchTerms = $('#search-terms').val();
+    $('#search-terms').val("");
+    
+    let request = new XMLHttpRequest();
+    const url = `http://api.giphy.com/v1/gifs/${searchTerms}&api_key=${process.env.API_KEY}&limit=20`
+    
+    request.onreadystatechange = () => {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      // $('#search-results').html(`some kind of HTML here ${this.data[0].images.fixed_height}`);
+    }
+  });
+});
